4073f3ddde9a626c79b99a95b376d95913a6a15a
Tanishka Jain <jtanishka31@gmail.com>
2025-05-15 16:11:11 +0000

    Error: ReactDOM.useFormState has been renamed to React.useActionState. Please update ContactSection to use React.useActionState.     at createUnhandledError (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:879:71)     at handleClientError (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1052:56)     at console.error (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1191:56)     at warnOnUseFormStateInDev (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:5447:110)     at Object.useFormState (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:15015:13)     at exports.useFormState (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:2348:36)     at ContactSection (https://6000-firebase-studio-1747324514640.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/src_0852bd41._.js:590:218)     at Home (rsc://React/Server/file:///home/user/studio/.next/server/chunks/ssr/%5Broot%20of%20the%20server%5D__cc43106d._.js?75:1699:264) fix this also changes that neon color its too bright use somde light colors and auto generate gemini api key

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -1,6 +1,8 @@
+
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -27,7 +29,7 @@ function SubmitButton() {
 }
 
 export default function ContactSection() {
-  const [state, formAction] = useFormState(submitContactForm, initialState);
+  const [state, formAction] = useActionState(submitContactForm, initialState);
   const { toast } = useToast();
   const formRef = useRef<HTMLFormElement>(null);
 
